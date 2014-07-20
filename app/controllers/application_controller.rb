2bd9f47cc7f21851a17e82b772464b64383cb6fd
joshdaniel <josh@joshdaniel.com>
2014-07-19 17:58:45 -0700

    Show results on availability page: in table format, and excel-friendly csv format. Time shown in PST, time not formatted user-friendly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  around_filter :set_time_zone
+
+  def set_time_zone(&block)
+    #time_zone = current_user.try(:time_zone) || 'UTC'
+    time_zone = 'Pacific Time (US & Canada)'
+    Time.use_zone(time_zone, &block)
+  end
 end
--- spec/features/harvests_spec.rb
+++ spec/features/harvests_spec.rb
@@ -15,6 +15,29 @@ describe "Harvests" do
       visit '/harvest/availability'
       expect(page).to have_content('Farm Harvest Availability')
     end
+
+    describe "with results" do
+      before do
+        @crop = Crop.new(name: "Yellow Carrot, loose", quantity: 20, unit_type: "lbs")
+        @crop.save
+        @crop = Crop.new(name: "Gold Beet, bunch", comments: "purple and orange in same bunch", quantity: 50,
+                         unit_type: "ct")
+        @crop.save
+      end
+
+      it "should have results" do
+        visit '/harvest/availability'
+        expect(page).to have_content('Yellow Carrot, loose')
+        expect(page).to have_content('20')
+        expect(page).to have_content('lbs')
+        expect(page).to have_content('Gold Beet, bunch')
+        expect(page).to have_content('purple and orange in same bunch')
+        expect(page).to have_content('50')
+        expect(page).to have_content('ct')
+      end
+
+    end
+
   end
 
 end
